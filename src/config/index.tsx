79627894cbd9c361baaec39533e6a0c7b610d76a
Rustaname254 <ofury47@gmail.com>
2025-08-18 19:41:31 +0300

    wallet connect project id added

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,7 +2,7 @@ import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { scroll, scrollSepolia } from '@reown/appkit/networks'
 
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
 if (!projectId) {
   throw new Error('Project ID is not defined')
 }
